a7af6d1f0bc11288a0c9f4334fd208e53344fded
Jere <77407960+Narkomaani@users.noreply.github.com>
2023-07-20 15:05:04 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const tableBody = document.getElementById("table-body");
+
+async function getTable() {
+  const url =
+    "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
+  const tablePromise = await fetch(url);
+  const tableJson = await tablePromise.json();
+  let labels = Object.values(tableJson.dataset.dimension.Alue.category.label);
+
+  // now that i found the object.values u could use forEach but this already works
+  for (let i = 0; i < tableJson.dataset.value.length; i++) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+
+    td1.innerText = labels[i];
+    td2.innerText = tableJson.dataset.value[i];
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tableBody.appendChild(tr);
+  }
+}
+
+getTable();
